4af1edf5cb04f04f2847eaecae3853b26e561f18
Omar Houmz <omar.houmz@schibsted.com>
2019-04-17 14:59:15 +0100

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,32 @@
-import React, { Component } from 'react';
-import '../LineApp.css';
+import React, { Component } from "react";
+import "../LineApp.css";
 
 class Footer extends Component {
   render() {
     return (
-        <footer className="page-footer transparent">
-            <div className="footer-copyright teal darken-2">
-                <div className="container">
-                    © 2017 Copyright to <a className="grey-text text-lighten-4" href="https://github.com/omhoumz">@omhoumz</a>
-                    <a className="grey-text text-lighten-4 right" href="mailto://omarhoumz@gmail.com">omarhoumz@gmail.com</a>
-                </div>
-            </div>
-        </footer>
+      <footer className="page-footer transparent">
+        <div className="footer-copyright teal darken-2">
+          <div className="container">
+            <p>
+              © 2019 By{" "}
+              <a
+                className="grey-text text-lighten-4"
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://github.com/omhoumz"
+              >
+                @omhoumz
+              </a>
+              <a
+                className="grey-text text-lighten-4 right"
+                href="mailto://omarhoumz@gmail.com"
+              >
+                omarhoumz@gmail.com
+              </a>
+            </p>
+          </div>
+        </div>
+      </footer>
     );
   }
 }
