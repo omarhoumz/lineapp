e92c6590a313f6cfc1ac99607536e27114176156
Omar Houmz <omar.houmz@schibsted.com>
2019-04-17 15:06:59 +0100

    edit header style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,19 @@
-import React, { Component } from 'react';
-import '../LineApp.css';
+import React, { Component } from "react";
+import "../LineApp.css";
 
 class Header extends Component {
-    render() {
-        return (
-            <header className="page-header z-depth-3">
-                <div className="container">
-                    <h1 className="center-align">Welcome to <strong>LineApp</strong></h1>
-                    <h5 className="center-align">Add your tasks and todos to get organized.</h5>
-                </div>
-            </header>
-        );
-    }
+  render() {
+    return (
+      <header className="page-header z-depth-3">
+        <div className="container">
+          <h1>
+            Welcome to <strong>LineApp</strong>
+          </h1>
+          <h5>Add your tasks and todos to get organized.</h5>
+        </div>
+      </header>
+    );
+  }
 }
 
 export default Header;
