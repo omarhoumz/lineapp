0142f855c4742705b07929605463fbcf9e08253e
Omar Houmz <omar.houmz@schibsted.com>
2019-06-08 15:46:53 +0000

    add a navbar

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -7,43 +7,59 @@ import './header.css'
 const Header = ({ displayName, onClickSignOut, imageUrl }) => {
   return (
     <header className="page-header border-bottom border-light">
-      <div className="container d-flex flex-sm-row align-items-md-center flex-column justify-content-between py-5">
-        <div className="header-brand">
-          <h1>
-            LineApp
-            <button
-              type="button"
-              className="btn btn-link about-link"
-              data-toggle="modal"
-              data-target="#aboutLineApp"
-            >
-              About
-            </button>
-          </h1>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="navbar-brand header-brand">LineApp</div>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
 
-          <h2 className="h4">{displayName ? `${displayName}, ` : ''}Welcome</h2>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              {/* <a className="nav-link" href="#">
+                Home <span className="sr-only">(current)</span>
+              </a> */}
+              <button
+                type="button"
+                className="nav-link btn btn-link about-link"
+                data-toggle="modal"
+                data-target="#aboutLineApp"
+              >
+                About
+              </button>
+            </li>
+          </ul>
+          <div className="form-inline my-2 my-lg-0">
+            {imageUrl && (
+              <img
+                src={imageUrl}
+                alt={displayName}
+                width="32px"
+                height="32px"
+                style={{
+                  marginRight: '0.4rem',
+                }}
+                className="rounded-circle"
+              />
+            )}
+            {displayName && <h2 className="h6 display-name">{displayName}</h2>}
+            <button
+              className="btn btn-sm btn-outline-secondary"
+              onClick={onClickSignOut}
+            >
+              Sign Out
+            </button>
+          </div>
         </div>
-        <div className="header-account">
-          {imageUrl && (
-            <img
-              src={imageUrl}
-              alt={displayName}
-              width="48px"
-              height="48px"
-              style={{
-                marginRight: '1rem',
-              }}
-              className="rounded-circle"
-            />
-          )}
-          <button
-            className="btn btn-sm btn-outline-secondary"
-            onClick={onClickSignOut}
-          >
-            Sign Out
-          </button>
-        </div>
-      </div>
+      </nav>
       {/* About modal */}
       <About />
     </header>
